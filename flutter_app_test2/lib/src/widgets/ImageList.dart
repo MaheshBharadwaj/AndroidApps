f09459355a522a86547bad5657ab7cb9ebda3380
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2019-07-28 19:03:37 +0530

    Added Title

--- flutter_app_test2/lib/src/widgets/ImageList.dart
+++ flutter_app_test2/lib/src/widgets/ImageList.dart
@@ -6,23 +6,40 @@ class ImageList extends StatelessWidget{
     final List<ImageModel> images;
     ImageList(this.images);
 
+
+    Widget buildImage(ImageModel image){
+      return Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(3.5)),
+              border: Border.all(
+                color: Colors.white,
+                style : BorderStyle.solid,
+                width: 4.0,
+              )
+          ),
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.all(20.0),//All - all sides
+          child : Column(
+            children: <Widget>[
+              Padding(
+                child: Image.network(image.url),
+                padding: EdgeInsets.only(bottom: 8.0),
+              ),
+              Text(
+              image.title,
+              style: TextStyle(color: Colors.white),
+              ),
+            ],
+          )
+
+      );
+    }
+
     Widget build(context){
         return ListView.builder(
           itemCount: images.length,
           itemBuilder: (context,int index){
-              return Container(
-                      decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(3.5)),
-                                  border: Border.all(
-                                    color: Colors.white,
-                                    style : BorderStyle.solid,
-                                    width: 4.0,
-                                  )
-                      ),
-                      padding: EdgeInsets.all(10.0),
-                      margin: EdgeInsets.all(20.0),//All - all sides
-                      child : Image.network(images[index].url)
-              );
+              return buildImage(images[index]);
           },
         );
     }
