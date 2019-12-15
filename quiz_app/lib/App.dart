f1ff772a3a50e898d4d49ff7ec574767ad7925ef
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2019-12-15 22:13:27 +0530

    Udemy App

--- quiz_app/lib/App.dart
+++ quiz_app/lib/App.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget{
-    @override
-    Widget build(BuildContext ctx){
-        return MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text("My First APP",textAlign: TextAlign.center,style: TextStyle(color: Colors.white70)),
-              centerTitle: true,
-              backgroundColor: Colors.redAccent,
-            ),
-            backgroundColor: Colors.black,
-            body: Text('Text in the body',
-                       textAlign: TextAlign.center,
-                       style: TextStyle(color: Colors.white70,fontSize: 22)
-            )
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My First APP",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white70),
           ),
-        );
-    }
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        backgroundColor: Colors.black,
+        body: Container(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'Text in the body',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 22,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
