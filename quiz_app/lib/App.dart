02798d25bece04ec5968751c7e71c0d081e220ad
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2019-12-16 21:01:00 +0530

    Udemy App

--- quiz_app/lib/App.dart
+++ quiz_app/lib/App.dart
@@ -1,32 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "My First APP",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white70),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My First APP",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white70),
         ),
-        backgroundColor: Colors.black,
-        body: Container(
-          padding: EdgeInsets.all(15.0),
-          child: Text(
-            'Text in the body',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white70,
-              fontSize: 22,
-            ),
-          ),
-        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
       ),
-    );
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Question: ',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 25.0,
+              ),
+            ),
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text(
+                'Question 1',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 15.0),
+              ),
+              onPressed: null,
+            ),
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text(
+                'Question 2',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 15.0),
+              ),
+              onPressed: null,
+            ),
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text(
+                'Question 3',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 15.0),
+              ),
+              onPressed: null,
+            ),
+          ],
+        ),
+        padding: EdgeInsets.all(10.0),
+      ),
+    ));
   }
 }
