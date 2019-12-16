b54a00a0609afebf5bd0da666a68da52a61e7b69
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2019-12-16 22:13:11 +0530

    Minor Changes

--- quiz_app/lib/App.dart
+++ quiz_app/lib/App.dart
@@ -21,58 +21,57 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext ctx) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "My First APP",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white70),
+      appBar: AppBar(
+        title: Text(
+          "My First APP",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white70),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text(
+              questions[index],
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 25.0,
+              ),
             ),
-            centerTitle: true,
-            backgroundColor: Colors.redAccent,
-          ),
-          backgroundColor: Colors.black,
-          body: Container(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  questions[index],
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 25.0,
-                  ),
-                ),
-                RaisedButton(
-                  color: Colors.blueAccent,
-                  child: Text(
-                    'Question 1',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white70, fontSize: 15.0),
-                  ),
-                  onPressed: buttonClicked,
-                ),
-                RaisedButton(
-                  color: Colors.blueAccent,
-                  child: Text(
-                    'Question 2',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white70, fontSize: 15.0),
-                  ),
-                  onPressed: buttonClicked,
-                ),
-                RaisedButton(
-                  color: Colors.blueAccent,
-                  child: Text(
-                    'Question 3',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white70, fontSize: 15.0),
-                  ),
-                  onPressed: buttonClicked,
-                ),
-              ],
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text(
+                'Question 1',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 15.0),
+              ),
+              onPressed: buttonClicked,
             ),
-            padding: EdgeInsets.all(10.0),
-          ),
-        )
-    );
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text(
+                'Question 2',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 15.0),
+              ),
+              onPressed: buttonClicked,
+            ),
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text(
+                'Question 3',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 15.0),
+              ),
+              onPressed: buttonClicked,
+            ),
+          ],
+        ),
+        padding: EdgeInsets.all(10.0),
+      ),
+    ));
   }
 }
