4835d119da60f5349f586a67f3959d5621389e13
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2019-12-23 22:21:49 +0530

    Expenses APP

---
+++ expenses_tracker/lib/Transaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+//For @required
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
